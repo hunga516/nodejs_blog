1715175e669b590e50caef7cef1df0563d9754a9
hunga516 <hunga5160@gmail.com>
2024-07-30 00:24:25 +0700

    count deleted documents

--- blog_nl/blog/src/app/controllers/MeCoursesController.js
+++ blog_nl/blog/src/app/controllers/MeCoursesController.js
@@ -5,18 +5,18 @@ const { mutipleMongooseToObject, singleMongooseToObject } = require('../../utils
 class MeController {
     // [GET] /me/courses
     async meCourses(req, res, next) {
-        Course.find({})
-            .then(courses => {
-                res.render(`me/meCourses`, {
-                    courses: mutipleMongooseToObject(courses)
-                })
-            })
+
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) => res.render('me/meCourses', {
+                deletedCount,
+                courses: mutipleMongooseToObject(courses)
+            }))
             .catch(next)
     }
 
     // [GET] /me/courses/edit/:id
     async meEditCourse(req, res, next) {
-        Course.findById({ _id: req.params.id })
+        Course.findById({ _id: req.params.id })  //original method
             .then(course => {
                 res.render(`me/meEditCourse`, {
                     course: singleMongooseToObject(course)
@@ -26,34 +26,35 @@ class MeController {
     }
     // [PUT] /me/courses/edit/store/:id
     async storeEditCourse(req, res, next) {
-        Course.updateOne({ _id: req.params.id }, req.body)
+        Course.updateOne({ _id: req.params.id }, req.body)  //original method
             .then(course => res.redirect('/me/courses'))
             .catch(next)
     }
     // [DELETE] /me/courses/delete/:id
     async deleteCourse(req, res, next) {
-        Course.delete({ _id: req.params.id })
+        Course.delete({ _id: req.params.id })    //plugin mongoose-soft-delete method
             .then(course => res.redirect('back'))
             .catch(next)
     }
 
     async trash(req, res, next) {
-        Course.findWithDeleted({ deleted: true })
+        Course.findWithDeleted({ deleted: true }) //all course can use
             .then(courses => res.render('me/trashCourses', {
                 courses: mutipleMongooseToObject(courses)
             }))
     }
 
     async forceDeleteCourse(req, res, next) {
-        Course.deleteOne({ _id: req.params.id })
+        Course.deleteOne({ _id: req.params.id }) //original method
             .then(course => res.redirect('back'))
     }
 
     async restore(req, res, next) {
-        Course.restore({ _id: req.params.id })
+        Course.restore({ _id: req.params.id }) //plugin mongoose-soft-delete method
             .then(course => res.redirect('back'))
             .catch(next)
     }
+
 }
 
 module.exports = new MeController();
--- blog_nl/blog/src/app/models/Course.js
+++ blog_nl/blog/src/app/models/Course.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const slug = require('mongoose-slug-generator');
+const slug = require('mongoose-slug-updater');
 const mongooseDelete = require('mongoose-delete');
 
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
-    name: { type: String, default: "Default name" },
+    name: { type: String, default: "Default name" }, //default will enable if type === undefined
     description: { type: String, default: "Default description" },
-    images: { type: String, default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC5V5g68dTYlES7tgY55eWdnJSChHU27m1kg&s" }
-    // slug: { type: String, slug: 'name', unique: true }
+    images: { type: String, default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC5V5g68dTYlES7tgY55eWdnJSChHU27m1kg&s" },
+    slug: { type: String, slug: 'name', unique: true }
 }, {
     timestamps: true
 });
